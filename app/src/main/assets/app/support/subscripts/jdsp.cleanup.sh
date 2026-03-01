8bb062544341b3a3c728bad5a90e4dd9c7da24fa
Bartłomiej Wołk <bartlomiejwolk@gmail.com>
2026-03-01 14:41:31 +0100

    G2 support

--- app/src/main/assets/app/support/subscripts/jdsp.cleanup.sh
+++ app/src/main/assets/app/support/subscripts/jdsp.cleanup.sh
@@ -2,11 +2,13 @@
 	SDIR="$1"
 #the mountpoint to the tmpfs tree
 	TMPFS="$SDIR/support/jdsp4rp5_tmpfs"
-#where soundfx libs reside
-	SOUNDFX_DIR=/vendor/lib/soundfx
+#G2 profile: fixed soundfx directory and config targets
+		SOUNDFX_DIR=/vendor/lib64/soundfx
+		AUDIO_POLICY_TARGET=/vendor/etc/audio/sku_cliffs_qssi/audio_policy_configuration.xml
+		AUDIO_EFFECTS_TARGET=/vendor/etc/audio/sku_cliffs/audio_effects.xml
 
 #cleanup
-	umount /vendor/etc/audio/audio_policy_configuration.xml
+	[ -f "$AUDIO_POLICY_TARGET" ] && umount "$AUDIO_POLICY_TARGET"
 	for m in $(mount |grep tmpfs | grep $(basename $TMPFS)| awk -F' on ' '{print $2}' | awk -F' type ' '{print $1}') ; do
 		umount -l "$m"
 	done
@@ -15,7 +17,7 @@
 		umount -l "$m"
 	done
 	
-	umount /vendor/etc/audio_effects.xml
+	[ -f "$AUDIO_EFFECTS_TARGET" ] && umount "$AUDIO_EFFECTS_TARGET"
 
 	umount /vendor/etc/acdbdata/MTP
 
@@ -26,7 +28,8 @@
 #restart audio system
 	killall -q audioserver
 	killall -q mediaserver
-	
-#restart(?) jamesdsp
-	pm disable james.dsp
-	pm enable james.dsp
+
+#Close global effect control session for manager consistency.
+am broadcast --user 0 -a android.media.action.CLOSE_AUDIO_EFFECT_CONTROL_SESSION \
+	--ei android.media.extra.AUDIO_SESSION 0 \
+	--es android.media.extra.PACKAGE_NAME james.dsp
